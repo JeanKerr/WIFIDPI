ef4442746b4dbb518131de092a5fd857f7ba92c4
Jean.Kerr <coco.ke@ruhaoyi.com>
2016-10-12 23:33:44 +0800

    add files

---
+++ wifidog-1.3.0/src/agent_thread.h
@@ -0,0 +1,38 @@
+/* vim: set sw=4 ts=4 sts=4 et : */
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/* $Id$ */
+/** @file comm_agent_thread.h
+    @brief Periodically checks in with the central auth server so the auth
+    server knows the gateway is still up.  Note that this is NOT how the gateway
+    detects that the central server is still up.
+    @author Copyright (C) 2016 Coco.ke <coco.ke@ruhaoyi.com>
+*/
+
+#ifndef _COMM_AGENT_THREAD_H_
+#define _COMM_AGENT_THREAD_H_
+
+#include <sys/select.h>
+
+/** @brief Periodically checks on the auth server to see if it's alive. */
+void thread_comm_agent(void *arg);
+
+#endif
---
+++ wifidog-1.3.0/src/dpi_thread.c
@@ -0,0 +1,81 @@
+/* vim: set sw=4 ts=4 sts=4 et : */
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/* $Id$ */
+/** @file portal_thread.c
+    @brief Periodically checks in with the central portal server checks on 
+    the portal server to see if local portal version is up-to-date.
+    @author Copyright (C) 2004 Alexandre Carmel-Veilleux <acv@miniguru.ca>
+*/
+
+#include "common.h"
+#include "debug.h"
+#include "dpi_thread.h"
+
+extern int dpi_main(int argc, char **argv);
+extern int RunRhyDpi;
+
+void thread_comm_dpi(void *arg)
+{
+    int argc=10;
+    char* argv[10]={"RhyDpi", "-i", NULL/*eth0*/, "-f", NULL/*"udp port 53"*/, "-w", "/dev/null", "-g", "0", "-q"};
+    T_DPI_PARAM* DpiParam = arg;
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
+    struct timespec timeout;
+    
+    while (1) {
+        RunRhyDpi = DpiParam->dpiFlag;
+        if(RunRhyDpi)
+        {
+            argv[2]=DpiParam->portName;
+            argv[4]=DpiParam->bpfFilter;
+            argv[6]=DpiParam->logPath;
+            debug(LOG_INFO, "%s %s:  %s %s %s %s %s %s %s %s %s\n", 
+                           RunRhyDpi ? "Start" : "Stop", 
+                           argv[0], argv[1], argv[2], argv[3], argv[4], 
+                           argv[5], argv[6], argv[7], argv[8], argv[9]);
+            dpi_main(argc, argv);
+        }
+        /* Sleep for 10 seconds... */
+        timeout.tv_sec = time(NULL) + 60;
+        timeout.tv_nsec = 0;
+
+        /* Mutex must be locked for pthread_cond_timedwait... */
+        pthread_mutex_lock(&cond_mutex);
+
+        /* Thread safe "sleep" */
+        pthread_cond_timedwait(&cond, &cond_mutex, &timeout);
+
+        /* No longer needs to be locked */
+        pthread_mutex_unlock(&cond_mutex);
+    }
+
+    while(1)
+    {
+
+    }
+}
+
+
+
+
+
